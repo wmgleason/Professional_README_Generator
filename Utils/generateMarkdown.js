0a0576ba8cd834027574ad14e2a7f1ed68ad0728
Wanda Gleason <oglissane@gmail.com>
2021-02-20 01:34:30 -0500

    Added code to handle the badges

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -17,4 +17,40 @@ function generateMarkdown(data) {
 `;
 }
 
+// function to generate markdown for README
+const generateMarkdown = (data) => {
+
+  //License badge handling
+  // note to self - try using switch as in 09 math activity
+  switch (data.license) {
+    case 'Apache 2.0':
+      licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    case 'BSD 2-Clause':
+      licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+      break;
+    case 'BSD 3-Clause':
+      licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+      break;
+    case 'GNU AGPLv3.0':
+      licenseBadge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+      break;
+    case 'GNU GPLv2.0':
+      licenseBadge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+      break;
+    case 'GNU GPLv3.0':
+      licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+      break;
+    case 'MIT':
+      licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      break;
+    case 'Mozilla Public 2.0':
+      licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+      break;
+    default:
+      break;
+  }
+
+
+
 module.exports = generateMarkdown;
