668b8e8a48680ea24d4d1f1f42de8b74a52a9ce1
Wanda Gleason <oglissane@gmail.com>
2021-02-21 22:00:44 -0500

    Trying soemthing new with the links

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -1,4 +1,3 @@
-
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -25,8 +24,7 @@ function generateMarkdown(response) {
   # Table of Contents
   ## Description
   ![License](https://img.shields.io/badge/License-${response.License}+blue.svg) "License Badge"
-
-    ${response.Description}
+  ${response.Description}
   ## Installation
   ${response.Installation}
   ## Usage:
@@ -42,8 +40,9 @@ function generateMarkdown(response) {
   ## GitHub username:
   ${response.GitHub}
   ## License
-    For more information on the license, click on the link below
-    - [License Info](http://opensource.org/licenses/${response.License})
+  For more information on the license, click on the link below
+  - [License Badge](http://opensource.org/licenses/${response.license})
+  // ![badge](https://img.shields.io/badge/license-${response.license}+blue.svg)
 
   ## Questions or comments?
     If you have any questions or would like to leave a comment, please see my GitHub page at:
@@ -51,38 +50,38 @@ function generateMarkdown(response) {
   
     Or send me an email at:
     ${response.Email}
-    `
+    `;
+}
   //License badge handling
   // note to self - try using switch as in 09 math activity
-  let licenseBadge;
-  switch (response.license) {
-    case 'Apache 2.0':
-      licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-      break;
-    case 'BSD 2-Clause':
-      licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
-      break;
-    case 'BSD 3-Clause':
-      licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
-      break;
-    case 'GNU AGPLv3.0':
-      licenseBadge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
-      break;
-    case 'GNU GPLv2.0':
-      licenseBadge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
-      break;
-    case 'GNU GPLv3.0':
-      licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
-      break;
-    case 'MIT':
-      licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-      break;
-    case 'Mozilla Public 2.0':
-      licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
-      break;
-    default:'None'
-      break;
-  };
-
+  // let licenseBadge;
+  // switch (response.license) {
+  //   case 'Apache 2.0':
+  //     licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  //     break;
+  //   case 'BSD 2-Clause':
+  //     licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+  //     break;
+  //   case 'BSD 3-Clause':
+  //     licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  //     break;
+  //   case 'GNU AGPLv3.0':
+  //     licenseBadge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+  //     break;
+  //   case 'GNU GPLv2.0':
+  //     licenseBadge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+  //     break;
+  //   case 'GNU GPLv3.0':
+  //     licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  //     break;
+  //   case 'MIT':
+  //     licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  //     break;
+  //   case 'Mozilla Public 2.0':
+  //     licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  //     break;
+  //   default:'None'
+  //     break;
+  // };
 
 module.exports = generateMarkdown;
